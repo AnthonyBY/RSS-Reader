ac64d5a9b8368c345948fb2ceff4669f59aa448c
Anthony Marchenko <anthonymarchenko@gmail.com>
2014-01-06 04:31:23 +0300

    Initial Commit

---
+++ RSS Reader/RSS Reader/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  RSS Reader
+//
+//  Created by Anthony Marchenko on 1/6/14.
+//  Copyright (c) 2014 Anthony Marchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
